4f08a18561d1b1053fae555b75deab121b799e55
Borregaard <frederik.borregaard@gmail.com>
2022-09-14 17:34:00 +0200

    Removed unused code

--- main.c
+++ main.c
@@ -2,36 +2,16 @@
 #include <Windows.h>
 #include <time.h>
 
-void clock_v1();
 void clock_v2();
 
 int main() {
-    // clock_v1();
+
     for (int i = 0; i < 60; ++i) {
         clock_v2();
         Sleep(1000);
     }
 
     return 0;
-}
-
-void clock_v1() {
-    time_t start, end;
-
-    for (int h = 0; h < 24; ++h) {
-        for (int m = 0; m < 60; ++m) {
-            start = time(NULL);
-
-            for (int s = 0; s < 60; ++s) {
-                printf("%d h %d m %d s \n", h, m, s);
-                Sleep(1000);
-            }
-
-            end = time(NULL);
-            printf("Time taken to print the clock is %.2f seconds \n",
-                   difftime(end, start));
-        }
-    }
 }
 
 void clock_v2() {
